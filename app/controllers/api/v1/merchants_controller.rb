4992247e921d42a4f7f55bd86b7a3fe8400d393b
Kelsie Porter <krporter22@gmail.com>
2023-02-07 12:15:14 -0700

    Create merchant show action

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -2,4 +2,18 @@ class Api::V1::MerchantsController < ApplicationController
   def index
     render json: MerchantSerializer.new(Merchant.all)
   end
+
+  def show
+    begin
+      merchant = Merchant.find(params[:id])
+      render json: MerchantSerializer.new(merchant)
+    rescue StandardError => e
+      error_merchant = ErrorMerchant.new(
+        e.message, 
+        "NOT FOUND",
+        404
+      )
+      render json: ErrorMerchantSerializer.new(error_merchant).serialized_json
+    end
+  end
 end
