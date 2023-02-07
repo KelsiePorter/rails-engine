9ec679ebf0efdd010dd17a5e2cee9b1f793807ce
Kelsie Porter <krporter22@gmail.com>
2023-02-07 13:01:00 -0700

    Create items show endpoint tests

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -37,4 +37,37 @@ describe 'Items API' do
     expect(items).to have_key(:data)
     expect(items[:data].size).to eq(0)
   end
+
+  it 'can get one item by its id' do 
+    merchant = create(:merchant)
+    item = create(:item, merchant_id: merchant.id)
+
+    get "/api/v1/items/#{item.id}"
+
+    item_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(item_response[:data]).to have_key(:id)
+    expect(item_response[:data]).to have_key(:type)
+    expect(item_response[:data][:type]).to eq("item")
+    expect(item_response[:data]).to have_key(:attributes)
+    expect(item_response[:data][:attributes]).to have_key(:description)
+    expect(item_response[:data][:attributes][:description]).to eq(item.description)
+    expect(item_response[:data][:attributes]).to have_key(:unit_price)
+    expect(item_response[:data][:attributes][:unit_price]).to eq(item.unit_price)
+  end
+
+  it 'returns an error response when the item id does not exist' do 
+
+    get "/api/v1/items/1"
+
+    item_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(item_response[:error]).to have_key(:status)
+    expect(item_response[:error][:status]).to eq("NOT FOUND")
+    expect(item_response[:error]).to have_key(:message)
+    expect(item_response[:error][:message]).to eq("Couldn't find Item with 'id'=1")
+    expect(item_response[:error]).to have_key(:code)
+    expect(item_response[:error][:code]).to eq(404)
+  end
 end
