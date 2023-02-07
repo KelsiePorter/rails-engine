5a02cfe0670676c95d0bbd64a7fdcec4436fb6c4
Kelsie Porter <krporter22@gmail.com>
2023-02-06 20:41:53 -0700

    Complete tests for items index

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -13,18 +13,28 @@ describe 'Items API' do
 
     items = JSON.parse(response.body, symbolize_names: true)
 
-    expect(items.count).to eq(3)
+    expect(items).to have_key(:data)
+    expect(items[:data].size).to eq(3)
 
-    items.each do |item|
+    items[:data].each do |item|
       expect(item).to have_key(:id)
-      expect(item[:id]).to be_an(Integer)
-      expect(item).to have_key(:description)
-      expect(item[:description]).to be_a(String)
-      expect(item).to have_key(:unit_price)
-      expect(item[:unit_price]).to be_a(Float)
-      expect(item).to have_key(:merchant_id)
-      expect(item[:merchant_id]).to be_an(Integer)
-      expect(item[:merchant_id]).to eq(merchant.id)
+      expect(item).to have_key(:type)
+      expect(item[:type]).to eq("item")
+      expect(item).to have_key(:attributes)
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes]).to_not have_key(:merchant_id)
     end
+  end
+
+  it 'returns an empty array when there are no items' do 
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(items).to have_key(:data)
+    expect(items[:data].size).to eq(0)
   end
 end
