9514dacdf337c3bd95a0ea240b2c7ea03f1ca2ae
Kelsie Porter <krporter22@gmail.com>
2023-02-08 15:17:11 -0700

    Create merchant items index and resuce block

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do 
       resources :items
-      resources :merchants, only: [:index, :show]
+      resources :merchants, only: [:index, :show] do
+        resources :items, only: [:index], controller: 'merchants/items'
+      end
     end
   end
 end
